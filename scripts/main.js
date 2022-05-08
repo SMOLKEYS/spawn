87e526970dbb00d61e57238407cd426cf57d82ec
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-05-08 23:36:53 +0800

    t

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,9 @@ const player = Vars.player
 global.spawn = (unit, team) => {
     unit.spawn(team, player.x, player.y);
 };
+
+global.spawnMany = (unit, team, count) => {
+    for(let i = 0; i < count; i++){
+        unit.spawn(team, player.x, player.y);
+    };
+};
